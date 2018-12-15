105895da0f7694f246279e41721c2d21722e4233
MeraryMC <m_martinez_cobian@berkeley.edu>
2018-12-15 15:37:33 -0800

    implemented sum and arraySum using recursion

--- src/recursion.js
+++ src/recursion.js
@@ -15,14 +15,31 @@ var factorial = function(n) {
 	return n * factorial( n - 1 );
 	}
 };
+
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+	if (array.length === 0){
+		return 0;
+	} else if (array.length === 1){
+		return array[0];
+	} else {
+		return array[0] + sum(array.slice(1));
+	}
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+	var count = 0;
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] instanceof Array) {
+			count += arraySum(array[i]);
+		}
+		if (array[i] === Math.round(array[i])) {
+			count += array[i];
+		}
+	} return count;
 };
 
 // 4. Check if a number is even.
